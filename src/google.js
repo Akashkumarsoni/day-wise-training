61adcadfaab2e3561aadac0a4c83eed9cf71800d
akashsoni0786 <akashsoni@cedcoss.com>
2022-12-09 19:11:49 +0530

    Day3 Task

--- src/google.js
+++ src/google.js
@@ -4,25 +4,25 @@ import "./App.css";
 import { dataTaxonomy } from "./data";
 
 function GoogleTaxonomy() {
-  const txttoarr = dataTaxonomy.split("\n");
-  const d2arr = txttoarr.map((item) => item.split(" > "));
-  const a = d2arr.filter((item) => item.length === 1);
+  const strArr = dataTaxonomy.split("\n");
+  const updatedArr = strArr.map((item) => item.split(" > "));
+  const a = updatedArr.filter((item) => item.length === 1);
   const [print, setPrint] = useState([a]);
   let arr = [];
-  for (let i = 0; i < d2arr.length; i++) {
-    for (let j = 0; j < d2arr[i].length; j++) {
-      let curr = d2arr[i][j];
-      if (arr[curr] === undefined) arr[curr] = [];
+  for (let i = 0; i < updatedArr.length; i++) {
+    for (let j = 0; j < updatedArr[i].length; j++) {
+      let current = updatedArr[i][j];
+      if (arr[current] === undefined) arr[current] = [];
     }
   }
-  for (let i = 0; i < d2arr.length; i++) {
-    for (let j = 1; j < d2arr[i].length; j++) {
-      let curr = d2arr[i][j - 1];
-      if (!arr[curr].includes(d2arr[i][j])) arr[curr].push(d2arr[i][j]);
+  for (let i = 0; i < updatedArr.length; i++) {
+    for (let j = 1; j < updatedArr[i].length; j++) {
+      let current = updatedArr[i][j - 1];
+      if (!arr[current].includes(updatedArr[i][j])) arr[current].push(updatedArr[i][j]);
     }
   }
 
-  const childrenHandler = (val, i) => {
+  const selectingEvent = (val, i) => {
     let arr1 = print.slice(0, i + 1);
     if (arr[val].length > 0) setPrint([...arr1, arr[val]]);
   };
@@ -32,7 +32,7 @@ function GoogleTaxonomy() {
      <div className="selectbox">
      {print.map((item, i) => (
         <select
-          onChange={(event) => childrenHandler(event.target.value, i)}
+          onChange={(event) => selectingEvent(event.target.value, i)}
           key={i}
         >
           <option disabled>select</option>
