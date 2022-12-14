ff1b19639a7915d1d569c9d68c3e9a9dcfcf60db
akashsoni0786 <akashsoni@cedcoss.com>
2022-12-14 14:51:42 +0530

    HOC in Clock

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,14 @@
-import { useState } from "react";
 import "./App.css";
 import Clock1 from "./clock1";
 import Clock2 from "./clock2";
 import Clock3 from "./clock3";
 
 function App() {
-  // const [currentHour, setCurrentHour] = useState(0);
-  // const [currentMinute, setCurrentMinute] = useState(0);
-  // const [currentSeconds, setCurrentSeconds] = useState(0);
-  // function time() {
-  //   var d = new Date();
-  //   setCurrentSeconds(d.getSeconds());
-  //   setCurrentMinute(d.getMinutes());
-  //   setCurrentHour(d.getHours());
-  // }
-  // setInterval(time, 1000);
-
   return (
     <div className="App">
       <Clock1/>
       <Clock2/>
-      <Clock3
-        // hours={currentHour}
-        // minutes={currentMinute}
-        // seconds={currentSeconds}
-      />
+      <Clock3/>
     </div>
   );
 }
--- src/HOC.tsx
+++ src/HOC.tsx
@@ -1,32 +1,28 @@
-import { ComponentType, useState } from "react";
+import { useState } from "react";
 
-const HOC = (IProps: ComponentType<any>) => {
+const HOC = (Children: any) => {
+  const DummyComp = (props: any) => {
+    const [currentHour, setCurrentHour] = useState(0);
+    const [currentMinute, setCurrentMinute] = useState(0);
+    const [currentSeconds, setCurrentSeconds] = useState(0);
+    function time() {
+      var d = new Date();
+      setCurrentSeconds(d.getSeconds());
+      setCurrentMinute(d.getMinutes());
+      setCurrentHour(d.getHours());
+    }
+    setInterval(time, 1000);
 
-  // var currentSeconds: number, currentMinute: number, currentHour: number;
-  // var d = new Date();
-  // function time() {
-  //   currentSeconds = d.getSeconds();
-  //   currentMinute = d.getMinutes();
-  //   currentHour = d.getHours();
-  // }
-
-  // const [currentHour, setCurrentHour] = useState(0);
-  // const [currentMinute, setCurrentMinute] = useState(0);
-  // const [currentSeconds, setCurrentSeconds] = useState(0);
-  // function time() {
-  //   var d = new Date();
-  //   setCurrentSeconds(d.getSeconds());
-  //   setCurrentMinute(d.getMinutes());
-  //   setCurrentHour(d.getHours());
-  // }
-  // setInterval(time, 1000);
-  return (props: any) => {
-    return <IProps 
-    {...props} 
-    hours={"currentHour"} 
-    minutes={"currentMinute"} 
-    seconds={"currentSeconds"} 
-    />;
+    return (
+      <>
+        <Children
+          hours={currentHour}
+          minutes={currentMinute}
+          seconds={currentSeconds}
+        />
+      </>
+    );
   };
+  return DummyComp;
 };
 export default HOC;
